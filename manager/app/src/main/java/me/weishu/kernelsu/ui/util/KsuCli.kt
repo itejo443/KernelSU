18188cba9a60b78a0246bf75cfaf9b3f42fbb2bb
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-12-30 16:38:43 +0800

    manager: remove shrink image

--- manager/app/src/main/java/me/weishu/kernelsu/ui/util/KsuCli.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/util/KsuCli.kt
@@ -245,10 +245,6 @@ fun uninstallPermanently(
     return FlashResult(result)
 }
 
-suspend fun shrinkModules(): Boolean = withContext(Dispatchers.IO) {
-    execKsud("module shrink", true)
-}
-
 @Parcelize
 sealed class LkmSelection : Parcelable {
     data class LkmUri(val uri: Uri) : LkmSelection()
